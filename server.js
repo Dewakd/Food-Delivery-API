44c0fb58b524f658c3269974fa987bbc77fbb1e4
Dewakd <86773325+Dewakd@users.noreply.github.com>
2025-04-15 01:23:07 +0800

    Membuat koneksi database menggunakan library mysql, dan melakukan uji coba dengan melakukan method get all user

--- server.js
+++ server.js
@@ -3,10 +3,29 @@ import swaggerUi from 'swagger-ui-express';
 import YAML from 'yamljs';
 import fs from 'fs';
 import mysql from 'mysql'
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 app.use(express.json());
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+const db = mysql.createConnection({
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE
+});
+
+db.connect((err) => {
+    if (err) {
+        console.error('Error connecting to the database:', err);
+        return;
+    }
+    console.log('Connected to the database');
+});
+
 
 const swaggerDocument = YAML.parse(fs.readFileSync('food-delivery-api.yml', 'utf8'));
 
@@ -16,6 +35,14 @@ app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
 
-
+app.get('/api/v1/users', (req, res) => {
+    db.query('SELECT * FROM users', (err, results) => {
+        if (err) {
+            res.status(500).send('Error getting users');
+            return;
+        }
+        res.json(results);
+    });
+});
 
 
